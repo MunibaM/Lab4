62b5b53a6c7ea282f574736b97645ac4b98f846b
MunibaM <149747234+MunibaM@users.noreply.github.com>
2023-11-13 02:56:53 +0000

    User Input Stuff

--- devdojo.sh
+++ devdojo.sh
@@ -1,7 +1,13 @@
  
 #!/bin/bash
 
-echo "Hello please" $1
+#!/bin/bash
+
+read -p"What is your name?" name
+
+echo "Hi there $name"
+echo "Welcome to DevDojo!"
+echo "Have a great Wednesday Night!"
 
 
 
