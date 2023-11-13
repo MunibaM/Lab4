d59c3a0ba4bb0f41483f7de6eee6e6c361002a91
MunibaM <149747234+MunibaM@users.noreply.github.com>
2023-11-13 02:58:53 +0000

    Added Comments

--- devdojo.sh
+++ devdojo.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 
 #!/bin/bash
-
+# Ask the user for their name
 read -p"What is your name?" name
-
+# Greet the user
 echo "Hi there $name"
 echo "Welcome to DevDojo!"
 echo "Have a great Wednesday Night!"
